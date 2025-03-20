b5626afb67c4e413ba7d3cd3bd8dbc0f2282419f
saraamin2 <sarah.amin22@hotmail.com>
2025-03-20 21:17:26 +0400

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GulfSymbolProject.Startup))]
+namespace GulfSymbolProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
